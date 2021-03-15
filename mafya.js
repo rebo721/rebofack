a797a6305952a92a0e46e8423e87f0aed42de674
MAFYA721 <70732308+MAFYA721@users.noreply.github.com>
2021-03-15 20:07:15 +0300

    Update and rename bot.js to mafya.js

--- mafya.js
+++ mafya.js
@@ -7387,9 +7387,9 @@ message.member.voiceChannel
 
 //تۆكینه‌كان لێره‌ جا بنێ
 
-client.login("ODAyOTAzODAxMDU2NzIyOTk0.YA2BEw.V293t8zqXgii5KBXOu2pWITkCFg");
-client2.login("ODAyOTA0NzA0Nzc0NDM4OTMy.YA2BjQ._p4CPQ4fcXcQ8ymZBecyqXPKiKE");
-client3.login("ODAyOTA1MTA5NTgyNTc3NzE0.YA2EPg.IzqvAA3_8S5647QKXM-DJjw6X9A");
+client.login("");
+client2.login("");
+client3.login("");
 client4.login("");
 client5.login("");
 client6.login("");
